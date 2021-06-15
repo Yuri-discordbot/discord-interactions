a2079aac1a43cbea57d8a75e40d86c8dea229b13
Catherine Gilbert <catherine.gilbert15@gmail.com>
2021-06-14 20:56:42 -0400

    cookin' some spaghett

--- src/interactions.js
+++ src/interactions.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
-const {InteractionResponseType, InteractionType, verifyKeyMiddleware} = require("discord-interactions");
+const {verifyKeyMiddleware} = require("discord-interactions");
 const helloData = require("data-access");
 
 const PUBLIC_DISCORD_APPLICATION_KEY = process.env.public_discord_application_key;
@@ -99,20 +99,69 @@ app.post('/', verifyKeyMiddleware(PUBLIC_DISCORD_APPLICATION_KEY), async (req, r
             data: data,
         });
     } else if (message.type === 3) {
-        console.log(message.message);
+        console.log(message);
+        //console.log(message.message);
+        //console.log(message.message.components[0].components);
 
-        res.send({
-            type: 7,
-            data: {
-                embeds: [
-                    {
-                        description: "button clicked",
-                        color: 16755938,
+        if (message.data !== undefined && (message.data.custom_id === "accept_offer" || message.data.custom_id === "decline_offer")) {
+            const clickUser = message.member !== undefined ? message.member.user : message.user;
+
+            let canUserClick = false;
+
+            const embedMessage = message.message.embeds[0].description;
+            const lastMentionInMessageStart = embedMessage.lastIndexOf("<@") + 2;
+            const lastMentionInMessageEnd = embedMessage.lastIndexOf(">");
+            const receiverMention = embedMessage.substring(lastMentionInMessageStart, lastMentionInMessageEnd);
+
+            if (lastMentionInMessageStart === 2) {
+                // @everyone was used maybe? i guess?
+                if (embedMessage.includes("@everyone")) {
+                    canUserClick = true;
+                }
+            } else {
+                if (receiverMention[0] === "&") {
+                    // a role has been tagged, check if user has role
+                    message.member.roles.forEach(role => {
+                        if (receiverMention.substring(1) === role) {
+                            // user that clicked has the mentionned role
+                            canUserClick = true;
+                        }
+                    });
+                } else {
+                    // a user has been tagged
+                    if (receiverMention === clickUser.id) {
+                        // user who clicked is the same that was tagged
+                        canUserClick = true;
                     }
-                ],
-                components: []
+                }
             }
-        });
+
+            if (canUserClick && message.data.custom_id === "accept_offer") {
+                res.send({
+                    type: 4,
+                    data: {
+                        embeds: [
+                            {
+                                description: `<@${message.message.interaction.user.id}> pats <@${clickUser.id}>`,
+                                image: {
+                                    url: "https://media1.tenor.com/images/d7c326bd43776f1e0df6f63956230eb4/tenor.gif?itemid=17187002"
+                                },
+                                color: 16755938,
+                            }
+                        ],
+                        components: [],
+                    }
+                });
+            } else {
+                res.send({
+                    type: 6,
+                });
+            }
+        } else {
+            res.send({
+                type: 6,
+            });
+        }
     }
 });
 
